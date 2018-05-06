88bd273bd4c130f048bb2cd892b76132e70b88a7
Michael Banzon <michael@banzon.dk>
2018-05-06 12:52:12 +0200

    Initual checkin

---
+++ sample.bash
@@ -0,0 +1,10 @@
+go run sample_random.go > random_sample0.csv
+go run sample_random.go > random_sample1.csv
+go run sample_random.go > random_sample2.csv
+go run sample_random.go > random_sample3.csv
+go run sample_random.go > random_sample4.csv
+go run sample_random.go > random_sample5.csv
+go run sample_random.go > random_sample6.csv
+go run sample_random.go > random_sample7.csv
+go run sample_random.go > random_sample8.csv
+go run sample_random.go > random_sample9.csv
